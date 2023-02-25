cf58560ee99236ca9fe6f7ac3d3e4a8955ece073
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-02-25 14:36:05 +0700

    ADD : Setting Up A Function To Call The API

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,7 +1,35 @@
+import 'dart:convert';
 import 'package:get/get.dart';
+import 'package:get/get_connect/connect.dart';
+import '../../../data/entertaimnet_response.dart';
+import '../../../data/headline_response.dart';
+import '../../../data/sports_response.dart';
+import '../../../data/technology_response.dart';
+import '../../../utils/api.dart';
 
 class DashboardController extends GetxController {
   //TODO: Implement DashboardController
+  final _getConnect = GetConnect();
+
+  Future<HeadlineResponse> getHeadline() async {
+    final response = await _getConnect.get(BaseUrl.headline);
+    return HeadlineResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<EntertaimentResponse> getEntertaiment() async {
+    final response = await _getConnect.get(BaseUrl.headline);
+    return EntertaimentResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<SportsResponse> getSports() async {
+    final response = await _getConnect.get(BaseUrl.headline);
+    return SportsResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<TechnologyResponse> getTechnology() async {
+    final response = await _getConnect.get(BaseUrl.headline);
+    return TechnologyResponse.fromJson(jsonDecode(response.body));
+  }
 
   final count = 0.obs;
   @override
