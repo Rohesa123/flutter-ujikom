8b65123021ff3fd4ba6e8c4fc3b9fcfea39fdf44
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-02-25 13:21:13 +0700

    ADD: Login Function

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,9 +1,17 @@
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoginController extends GetxController {
   //TODO: Implement LoginController
 
-  final count = 0.obs;
+  final _getConnect = GetConnect();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final authToken = GetStorage();
   @override
   void onInit() {
     super.onInit();
@@ -17,7 +25,35 @@ class LoginController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+    emailController.dispose();
+    passwordController.dispose();
   }
 
-  void increment() => count.value++;
+  @override
+  void loginNow() async {
+    var client = http.Client();
+    var response;
+
+    response = await client.post(
+      Uri.https('demo-elearning.smkassalaambandung.sch.id', 'api/login'),
+      body: {
+        'email': emailController.text,
+        'password': passwordController.text,
+      },
+    );
+
+    var decodedResponse =
+        jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>;
+    if (decodedResponse['success'] == true) {
+      authToken.write('token', decodedResponse['access_token']);
+      Get.offAllNamed('/home');
+    } else {
+      Get.snackbar('Error', decodedResponse['message'],
+          icon: const Icon(Icons.error),
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+          forwardAnimationCurve: Curves.bounceIn,
+          margin: const EdgeInsets.only(top: 10, left: 5, right: 5));
+    }
+  }
 }
